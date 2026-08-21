9177b51b960b4329d346f590dd969d62e1168916
Alon <miki20141110@gmail.com>
2026-08-21 19:27:11 +0800

    feat: setup_logger with level

--- infra-tracing/src/tests.rs
+++ infra-tracing/src/tests.rs
@@ -2,12 +2,12 @@ use crate::{Logger, LoggerGuard};
 use infra_core::config::BaseConfig;
 use tracing::Level;
 
-pub fn setup_logger() -> anyhow::Result<LoggerGuard> {
-	let mut bs_cfg = BaseConfig::default();
-	// bs_cfg.log_level = Some(Level::INFO);
-	let logger = Logger::default();
+pub fn setup_logger(level: Level) -> anyhow::Result<LoggerGuard> {
+    let mut bs_cfg = BaseConfig::default();
+    bs_cfg.log_level = Some(level);
+    let logger = Logger::default();
 
-	// init logger
-	let guard = logger.init(&bs_cfg);
-	Ok(guard)
+    // init logger
+    let guard = logger.init(&bs_cfg);
+    Ok(guard)
 }
