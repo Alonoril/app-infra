d09395f7f3ec3806da676fc448ae70c38bf0274e
Alon <miki20141110@gmail.com>
2026-08-31 13:28:26 +0800

    opt: infra-tracing setup_logger

--- infra-tracing/src/tests.rs
+++ infra-tracing/src/tests.rs
@@ -3,11 +3,11 @@ use infra_core::config::BaseConfig;
 use tracing::Level;
 
 pub fn setup_logger(level: Level) -> anyhow::Result<LoggerGuard> {
-    let mut bs_cfg = BaseConfig::default();
-    bs_cfg.log_level = Some(level);
-    let logger = Logger::default();
+	let mut bs_cfg = BaseConfig::default();
+	bs_cfg.log_level = Some(level);
+	let logger = Logger::default();
 
-    // init logger
-    let guard = logger.init(&bs_cfg);
-    Ok(guard)
+	// init logger
+	let guard = logger.init(&bs_cfg);
+	Ok(guard)
 }
